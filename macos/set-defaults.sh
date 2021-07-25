aa816ed099f0baeb7c04312416c1c22e83575a75
Luke Watts <luke@thisis.la>
2021-07-25 09:05:23 +0200

    ✨ configure how screenshots work

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -42,3 +42,8 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Save screengrabs to 
+mkdir -p ~/Screengrabs
+defaults write com.apple.screencapture location ~/Screengrabs
+defaults write com.apple.screencapture type jpg
